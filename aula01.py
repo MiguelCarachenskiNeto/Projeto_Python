e65516d84d5711a4f6025e6bf055d04bc017f2e5
juliam <juray2024@gmail.com>
2024-03-27 10:39:30 -0300

    Subir aula 1

---
+++ aula01.py
@@ -0,0 +1,10 @@
+import os
+#print("Olá Mundo")
+print("O 3DS é a turma número 1 do Sagrada")
+turma="3DS"
+pos=1
+
+print("O" ,turma, "é a turma número" ,pos, "do Sagrada")
+print(type (turma))
+print(type (pos))
+os.system("cis")
